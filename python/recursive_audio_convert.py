39b7300c0a2bd51b7173ff0c563d5f7abdd5b3ea
5zfxqlqgw9ru <devantriangga3@gmail.com>
2026-06-02 09:46:36 +0700

    add new script

--- python/recursive_audio_convert.py
+++ python/recursive_audio_convert.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python
-"""
-Batch convert and compress audio file reccurssively
-to use it first you need to change dir to target dir
-thats how i use it
-"""
 
 import subprocess
 import os
 from time import sleep
+
 
 def convert(
     input_file,
@@ -17,20 +13,13 @@ def convert(
     clean_metadata=False,
     extra_args=None,
     overwrite=True,
-    quiet=False,
-    dryrun=False
+    quiet=True,
+    execute=False,
 ):
-
-    """
-    Parse basic ffmpeg command for easy use
-    """
-    map_metadata = "0" if not clean_metadata else "-1"
-
-    # Base command
     command = ["ffmpeg"]
 
     if overwrite:
-        command.append("-y")  # auto overwrite
+        command.append("-y")
 
     if quiet:
         command.extend(["-hide_banner", "-loglevel", "error"])
@@ -42,6 +31,8 @@ def convert(
 
     if bitrate is not None:
         command.extend(["-b:a", bitrate])
+
+    map_metadata = "0" if not clean_metadata else "-1"
     command.extend(["-map_metadata", map_metadata])
 
     if extra_args:
@@ -49,175 +40,188 @@ def convert(
 
     command.append(output_file)
 
-    # Execute command
-    if dryrun:
-        print("[!] Will execute command :")
-        print(" ".join(command))
-        return
-    
-    subprocess.run(command)
+    result_data = {}
+    result_data["command"] = command
+    result_data["returncode"] = None
+    result_data["stdout"] = None
+
+    if execute:
+        result = subprocess.run(command)
+        result_data["returncode"] = result.returncode
+        result_data["stdout"] = result.stdout
+
+    return result_data
 
 
-def recurssive_convert(
-        dir_path, 
-        filter,
-        is_src_del,
-        extra={}):
-    """
-    Reccurssively convert by extension
-    """
+def get_files_list(dir_path, filter_ext):
+    file_list = []
 
-    extra_is_simple = extra.get("is_simple", True)
-    extra_quiet = extra.get("quiet", True)
-    extra_dryrun = extra.get("dryrun", False)
-    extra_codec = extra.get("codec", "aac")
-    extra_clean_metadata = extra.get("clean_metadata", False)
-    extra_bitrate = extra.get("bitrate", "330k")
-    extra_out_extension = extra.get("output_extension", None)
-
-    for root, _, files in os.walk(dir_path):
-        # Walk for file
+    for path, _, files in os.walk(dir_path):
         for file in files:
-            # Filter, is extension are matching
-            if file.endswith(filter):
-                print(f"[!] - {file}")
+            if file.endswith(filter_ext):
+                file_list.append(os.path.join(path, file))
 
-                # Create full path file source
-                file_source = os.path.join(root, file)
-
-                # Create full path file output
-                if extra_is_simple:
-                    output_extension = ".m4a"
-                else:
-                    if extra_out_extension is not None:
-                        _x = extra_out_extension
-                    else:
-                        _x = extra_codec
-                        if extra_codec is None:
-                            _x = filter
-                    output_extension = _x
-
-                out_file = f"{os.path.join(root, file).rstrip(filter)}_c.{output_extension}"
-
-                convert(
-                    input_file=file_source,
-                    output_file=out_file,
-                    quiet=extra_quiet,
-                    dryrun=extra_dryrun,
-                    codec=extra_codec,
-                    clean_metadata=extra_clean_metadata,
-                    bitrate=extra_bitrate
-                )
-
-                if is_src_del:
-                    sleep(0.1)
-                    os.remove(file_source)
+    return sorted(file_list)
 
 
-def user_interact_do_accept():
-    ui_confirm_action = input("Continue? (Y/n) ").lower()
-    if ui_confirm_action in ("n", "no"):
-        return 2
-    elif ui_confirm_action in ("y", "yes"):
-        return 1
+def ui_show_messages(message: dict, separator=":"):
+    max_len = max(len(k) for k in message)
+    keys = list(message.keys())
+
+    for i, key in enumerate(keys, 1):
+        value = message[key]
+        print(f"{i:>2}. {key:<{max_len}} {separator} {value}")
+
+
+def ui_input(prompt="> ", is_boolean=False, old_value=None):
+    user_input = input(prompt).strip()
+
+    if is_boolean:
+        if user_input.lower() in ("y", "yes", "true"):
+            return True
+        elif user_input.lower() in ("n", "no", "false"):
+            return False
+        elif user_input == "":
+            return old_value
+        else:
+            print("[!] input harus y/n")
+            return None
     else:
-        return 0
-
-
-def user_interact(is_more=False):
-    # Default option
-    is_source_delete = False
-    extension_filter = ".flac"
-    extra_argument = {
-        "codec" : "aac",
-        "bitrate" : "330k",
-        "clean_metadata" : False,
-        "dryrun" : False,
-        "quiet" : True,
-        "is_simple" : True
-    }
-
-    # User Input
-    ui_extension_filter = input("extension filter      ( Default .flac )  : ")
-    ui_is_source_delete = input("delete original file? ( Y/n, Default no) : ").lower()
-    
-    if is_more:
-        extra_argument["is_simple"] = False
-        ui_extra_codec      = input("Codec   [n]        (default AAC) : ").lower()
-        ui_extra_bitrate    = input("Bitrate [n]       (default 330k) : ").lower()
-        ui_extra_metadata   = input("Clean metadata       (default n) : ").lower()
-        ui_output_extension = input("Output extension (default codec) : ").lower()
-
-        if len(ui_extra_codec) > 1:
-            if ui_extra_codec == "none":
-                _x = None
-            else:
-                _x = ui_extra_codec
-            extra_argument["codec"] = _x
-        
-        if len(ui_extra_bitrate) > 1:
-            if ui_extra_bitrate == "none":
-                _y = None
-            else:
-                _y = ui_extra_bitrate
-            extra_argument["bitrate"] = _y
-
-        if len(ui_output_extension) > 0:
-            extra_argument["output_extension"] = ui_output_extension
-
-        extra_argument["clean_metadata"] = True if ui_extra_metadata in ("y", "yes") else False
-
-    # Check if extension filter are more than 1 char and apply if true
-    if len(ui_extension_filter) > 1:
-        extension_filter = ui_extension_filter
-
-    # Check if user agree, set is_source_delete to True
-    if ui_is_source_delete in ("y", "yes"):
-        is_source_delete = True
-
-    # Run but not execute
-    elif ui_is_source_delete == "dry":
-        is_source_delete = False
-        extra_argument["dryrun"] = True 
-
-    # Summary
-    print()
-    print("--- SUMMARY ---")
-    print("This will convert every file that have extension :", extension_filter)
-    if is_source_delete:
-        print("and will DELETE its original file")
-
-    if is_more:
-        print("and extra agrument")
-        print(extra_argument)
-
-    ui_is_accept = user_interact_do_accept()
-    if ui_is_accept == 2:
-        print("Abort")
-        return 2
-    if ui_is_accept != 1:
-        print("Invalid, retry")
-        return 1
-
-    recurssive_convert(
-        dir_path=".",
-        filter=extension_filter,
-        is_src_del=is_source_delete,
-        extra=extra_argument
-    )
-    return 0
+        if user_input == "":
+            return old_value
+        return user_input
 
 
 def main():
-    is_m = input("More advance option? Y/n (default n) : ").lower()
-    if is_m in ("y", "yes"):
-        print("More extra option, use 'none' to empty the value which will skip default value this only for [n] labeled")
-        is_m = True
-    else:
-        is_m = False
-    is_running = 1
-    while is_running == 1:
-        is_running = user_interact(is_more=is_m)
+    argument = {
+        "codec": "aac",
+        "bitrate": "330k",
+        "clean_metadata": False,
+        "delete source": False,
+        "extension original": ".flac",
+        "extension result": ".m4a",
+        "dryrun": False,
+    }
+
+    ui_show_messages(argument)
+    while True:
+        user_input = input("> ").strip()
+
+        # Input if its empty or done and end loops
+        if user_input in ("", "done"):
+            acc = ui_input(prompt="done? > ", is_boolean=True, old_value=False)
+            if acc:
+                break  # end loops
+            else:
+                continue
+
+        # Input if its q or exit and end programs
+        if user_input in ("q", "exit"):
+            acc = ui_input(prompt="exit? > ", is_boolean=True, old_value=False)
+            if acc:
+                print("Abort")
+                return  # exit
+            else:
+                continue
+
+        # show summary
+        if user_input in ("sum", "summary"):
+            ui_show_messages(argument)
+            continue
+
+        # if not input a number its skip current loop
+        if not user_input.isdigit():
+            print("[!] input must be a digit")
+            continue
+
+        # if input are digit, then this bellow are executed
+        idx = int(user_input)  # Turn user_input into a integer
+        keys = list(argument.keys())  # get a list of argument's key
+
+        # if input are more than argument, will skip current
+        if idx < 1 or idx > len(keys):
+            print("[!] out of range")
+            continue
+
+        key = keys[
+            idx - 1
+        ]  # get current input key, it devided by 1 bcause python start index from 0
+        current_value = argument[key]  # get current valeu
+
+        print(
+            f"Edit: {key} (current: {current_value})"
+        )  # to ensure myself using a right choice
+
+        # check is current edited key are booleans
+        is_bool = isinstance(current_value, bool)
+
+        new_value = ui_input(
+            prompt="value > ",
+            is_boolean=is_bool,
+            old_value=current_value,
+        )
+
+        if new_value is not None:
+            argument[key] = new_value
+            print(f"[✓] updated: {key} = {new_value}")
+        else:
+            print("[!] unchanged")
+
+    ui_show_messages(argument)
+    file_list = get_files_list(".", argument["extension original"])
+
+    if argument["extension result"][0] != 0:
+        argument["extension result"] = f".{argument['extension result']}"
+
+    if argument["extension original"][0] != 0:
+        argument["extension original"] = f".{argument['extension original']}"
+
+    if argument["codec"].lower() == "none":
+        argument["codec"] = None
+
+    if argument["bitrate"].lower() == "none":
+        argument["bitrate"] = None
+
+    execute = False if argument["dryrun"] else True
+
+    if argument["dryrun"]:
+        for input_file in file_list:
+            print("[!] Convert :", input_file)
+            if argument["delete source"]:
+                print("[!] unDelet :", input_file)
+
+        acc = ui_input(
+            prompt="continue [real] convert? > ", is_boolean=True, old_value=False
+        )
+        if not acc:
+            print("Abort")
+            return  # exit
+        else:
+            execute = True
+
+    for input_file in file_list:
+        output_file = f"{input_file.rstrip(argument['extension original'])}{argument['extension result']}"
+        print("[!] Convert :", input_file)
+
+        try:
+            result = convert(
+                input_file=input_file,
+                output_file=output_file,
+                codec=argument["codec"],
+                bitrate=argument["bitrate"],
+                clean_metadata=argument["clean_metadata"],
+                execute=execute,
+            )
+        except Exception as _:
+            print("[X] Fail convert :", input_file)
+            continue
+
+        is_convert_sucess = True if result["returncode"] == 0 else False
+        if argument["delete source"] and is_convert_sucess:
+            print("[!] Delete  :", input_file)
+            sleep(0.1)
+            os.remove(input_file)
+
 
 if __name__ == "__main__":
     main()
